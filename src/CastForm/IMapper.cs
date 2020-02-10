c3aa11318c7e372ad2efdb8aba3cbd7a23ecab43
Rafael Lillo <rafael.andrade@sky.uk>
2020-02-10 13:02:52 +0000

    Improve

--- src/CastForm/IMapper.cs
+++ src/CastForm/IMapper.cs
@@ -2,24 +2,59 @@
 
 namespace CastForm
 {
+    /// <summary>
+    /// Map source to destiny
+    /// </summary>
     public interface IMapper
     {
+        /// <summary>
+        /// Execute a mapping from source to destiny
+        /// </summary>
+        /// <typeparam name="TSource">Source type</typeparam>
+        /// <typeparam name="TDestiny">Destination type to create</typeparam>
+        /// <param name="source">Source object to map</param>
+        /// <returns>Mapped object</returns>
         TDestiny Map<TSource, TDestiny>(TSource source);
+
+        /// <summary>
+        /// Execute a mapping from source to destiny
+        /// </summary>
+        /// <typeparam name="TDestiny">Destination type to create</typeparam>
+        /// <param name="source">Source object to map</param>
+        /// <returns>Mapped object</returns>
         TDestiny Map<TDestiny>(object source);
     }
 
+    /// <summary>
+    /// Map source to destiny 
+    /// </summary>
     public interface IMap
     {
-
+        /// <summary>
+        /// Execute a mapping from source to destiny
+        /// </summary>
+        /// <param name="source">Source object to map</param>
+        /// <returns>Mapped object</returns>
         object Map(object source);
     }
 
+
+    /// <summary>
+    /// Map source to destiny 
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TDestiny"></typeparam>
     public interface IMap<TSource, TDestiny> : IMap
     {
         [return: MaybeNull]
         object IMap.Map(object source)
             => Map((TSource)source);
 
+        /// <summary>
+        /// Execute a mapping from source to destiny
+        /// </summary>
+        /// <param name="source">Source object to map</param>
+        /// <returns>Mapped object</returns>
         TDestiny Map(TSource source);
     }
 }
